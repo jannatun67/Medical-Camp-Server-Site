1968f0b36d36fd2ecfd3c3919e34fe55f6e0244b
jannatun67 <jannatunfima67@gmail.com>
2025-01-19 19:22:43 +0600

    post

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config(); // Load environment variables from .env file
-
+require('dotenv').config(); 
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,6 +29,40 @@ async function run() {
     const MedicalCampsCollection = client.db("Medical-Camp").collection("camps");
     const UserCollection = client.db("Medical-Camp").collection("users");
 
+
+    // jwt related api
+    app.post("/jwt", async(req,res)=>{
+      const user = req.body;
+      const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1h"});
+      res.send({token})
+    })
+    // Middleware
+     const verifyToken=(req,res,next)=>{
+       console.log("Inside verify token",req.headers.authorization);
+       if (!req.headers.authorization) {
+        return res.status(401).send({message: "unauthorized  access"})
+       }
+       const token = req.headers.authorization.split(' ')[1];
+       jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if (err) {
+          return res.status(401).send({message: "unauthorized  access"})
+        }
+        req.decoded = decoded
+         next()
+       })
+     }
+    // use verify admin after verifyToken
+    const verifyAdmin=async(req,res,next)=>{
+      const email = req.decoded.email;
+      const query={email:email};
+      const user= await UserCollection.findOne(query);
+      const isAdmin = user?.role === "admin"
+      if (!isAdmin) {
+        return res.status(403).send({message: "forbidden access"})
+      }
+      next()
+    }
+
     // MedicalCamps
     // get limit
     app.get("/medicalCamp",async(req,res)=>{
@@ -38,6 +72,7 @@ async function run() {
 
     // get
     app.get("/medicalCamps",async(req,res)=>{
+     
       const result = await MedicalCampsCollection.find().toArray();
       res.send(result)
     })
@@ -62,8 +97,6 @@ async function run() {
       res.send(result)
     })
     // Update
-    const { ObjectId } = require("mongodb");
-
     app.put("/medicalCamps/:id", async (req, res) => {
       const id = req.params.id;
     
@@ -121,6 +154,21 @@ async function run() {
       const result = await UserCollection.insertOne(user);
       // console.log(result);
       res.send(result)
+    })
+
+    app.get("/user/admin/:email",verifyToken,verifyAdmin, async (req,res)=>{
+      const email = req.params.email;
+      // console.log(email);
+      if (email !== req.decoded.email ) {
+        return res.status(403).send({message: "forbidden access"})
+      }
+      const query = {email:email}
+      const user = await UserCollection.findOne(query);
+      let admin = false
+      if (user) {
+        admin = user?.role === "admin"
+      }
+      res.send({admin})
     })
 
     // Send a ping to confirm a successful connection
